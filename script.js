072a355c4f24ce527169dcbf80561651121db3a0
Architha-Kavali <archithakavali4@gmail.com>
2026-03-08 18:24:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const words = ["Web Developer","Python Learner","Future Full Stack Developer"];
+
+let i = 0;
+let j = 0;
+let currentWord = "";
+let isDeleting = false;
+
+function type(){
+
+currentWord = words[i];
+
+if(isDeleting){
+j--;
+}else{
+j++;
+}
+
+document.querySelector(".typing").textContent = currentWord.substring(0,j);
+
+if(!isDeleting && j === currentWord.length){
+isDeleting = true;
+setTimeout(type,1000);
+return;
+}
+
+if(isDeleting && j === 0){
+isDeleting = false;
+i++;
+
+if(i === words.length){
+i = 0;
+}
+}
+
+setTimeout(type,100);
+}
+
+type();
